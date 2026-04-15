112c02cdd4c23eca89e8c1066ebbfaeedb9fa2a1
Devkumar-2305292 <142044460+Devkumar-2305292@users.noreply.github.com>
2026-04-15 19:42:26 +0530

    Add: valid-tree - submission-0

---
+++ Data Structures & Algorithms/valid-tree/submission-0.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean dfs(int node, int parent, List<List<Integer>> adj, boolean[] visited){
+        visited[node]=true;
+
+        for(int nei:adj.get(node)){
+            if(!visited[nei]){
+                if(!dfs(nei, node, adj, visited)){
+                    return false;
+                }
+            }else if(nei!=parent){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean validTree(int n, int[][] edges) {
+        if(n-1!=edges.length) return false;
+
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int[] edge:edges){
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+
+        boolean[] visited=new boolean[n];
+
+        if(!dfs(0, -1, adj, visited)){
+            return false;
+        }
+
+        for(boolean v:visited){
+            if(!v) return false;
+        }
+
+        return true;
+    }
+}
