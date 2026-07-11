1439929df7986aad14005a031beb315c29ffcec7
Devkumar-2305292 <142044460+Devkumar-2305292@users.noreply.github.com>
2026-07-11 13:41:04 +0530

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int helper(int i, int[] nums, int[] dp){
+        if(i>=nums.length){
+            return 0;
+        }
+
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+
+        int take=nums[i]+helper(i+2, nums, dp);
+        int notTake=helper(i+1, nums, dp);
+
+        return dp[i]=Math.max(take, notTake);
+    }
+
+    public int rob(int[] nums) {
+        int[] dp=new int[nums.length];
+        Arrays.fill(dp, -1);
+
+        return helper(0, nums, dp);
+    }
+}
